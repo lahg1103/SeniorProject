9798e403932f3314574f8a9bd30837a76e9d1c6c
nate <nathan.perez02@utrgv.edu>
2025-10-30 21:27:53 -0500

    added field for multiple travelers

--- context/fields.py
+++ context/fields.py
@@ -1,6 +1,7 @@
 
 from enum import Enum
 
+
 class FoodRestrictions(Enum):
     HALAL = 'Halal'
     KOSHER = 'Kosher'
@@ -8,21 +9,25 @@ class FoodRestrictions(Enum):
     VEGAN = 'Vegan'
     PESCATARIAN = 'Pescatarian'
 
+
 class TransportationType(Enum):
     RENTAL = 'Rental'
     UBER = 'Uber'
     PUBLICTRANSPORT = 'Public Transport'
     WALKING = 'Walking'
 
+
 class TripType(Enum):
     VACATION = 'Vacation'
     WORK = 'Work'
     STUDYABROAD = 'Study Abroad'
 
+
 class LodgingType(Enum):
     HOTEL = 'Hotel'
     AIRBNB = 'Airbnb'
     HOSTEL = 'Hostel'
+
 
 # stores template-facing data passed to Jinja templates.
 pages = [{'name': 'Home', 'endpoint': 'main.index'},
@@ -31,7 +36,9 @@ pages = [{'name': 'Home', 'endpoint': 'main.index'},
          {'name': 'Contact', 'endpoint': 'main.contact'}
          ]
 itineraryfields = [
-    {'name': 'Budget', 'type' : 'number', 'currency' : 'USD'},
+    {'name': 'Number of Travelers', 'type': 'number',
+        min: 1, max: 10, 'step': 1},
+    {'name': 'Budget', 'type': 'number', 'currency': 'USD'},
     {'name': 'Arrival Date', 'type': 'date'},
     {'name': 'Departure Date', 'type': 'date'},
     {'name': 'Destination', 'type': 'text'},
